c977db84f68bea26c1785d5f26a21dc49c4d226a
Tu Nombre <correo@example.com>
2025-04-04 18:26:59 -0500

    correccion Logo

--- client/src/components/MenuPrincipal.jsx
+++ client/src/components/MenuPrincipal.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import Logo from '../assets/logo.png';
+import Logo from '../assets/Logo.png';
 import './MenuPrincipal.css';
 
 function MenuPrincipal(){
