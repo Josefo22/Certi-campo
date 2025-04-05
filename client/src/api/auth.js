e5bd1096f9c90fdf85e6da99b7b3ca56ae5a2215
Tu Nombre <correo@example.com>
2025-04-04 19:58:49 -0500

    correccion rutas92

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,8 +1,8 @@
 import axios from './axios';
 
-export const loginRequest = user => axios.post(`/api/auth/login`, user);
+export const loginRequest = user => axios.post(`/auth/login`, user);
 
-export const verifyToken = (token) => axios.get(`/api/auth/verify`, {
+export const verifyToken = (token) => axios.get(`/auth/verify`, {
     headers: {
         Authorization: `Bearer ${token}`
     }
